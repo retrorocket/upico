f4ba1b0b6f65cfa2d21f054f33c3aa52db4420a2
retrorocket <unrealenemy@gmail.com>
2015-03-20 00:44:09 +0900

    delete type check

--- main.pl
+++ main.pl
@@ -8,16 +8,28 @@ use Net::Twitter::Lite::WithAPIv1_1;
 use Mojolicious::Lite;
 use File::Basename 'basename';
 use File::Path 'mkpath';
+use Config::Pit;
 
-my $consumer_key ="xxx";
-my $consumer_secret = "xxx";
+# Config::Pit
+my $config = Config::Pit::get("upico");
+
+my $consumer_key = $config->{consumer_key};
+my $consumer_secret = $config->{consumer_secret};
 
 my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 	consumer_key => $consumer_key,
 	consumer_secret => $consumer_secret,
 	ssl => 1
 );
-app->config(hypnotoad => {listen => ['http://*:xxx']});
+
+app->config(
+	hypnotoad => {
+		listen => ['http://*:'.$config->{port}],
+		heartbeat_timeout => 90,
+		inactivity_timeout => 90,
+	},
+);
+
 app->hook('before_dispatch' => sub {
 	my $self = shift;
 	if ($self->req->headers->header('X-Forwarded-Host')) {
@@ -42,7 +54,9 @@ get '/auth' => sub {
 	}
 
 	my $cb_url = $self->url_for('auth_cb')->to_abs->scheme('https');
-	my $url = $nt->get_authorization_url( callback => $cb_url );
+	#my $cb_url = $self->url_for('auth_cb')->to_abs->scheme('http');
+	#my $url = $nt->get_authorization_url( callback => $cb_url );
+	my $url = $nt->get_authentication_url( callback => $cb_url );
 
 	$self->session( token => $nt->request_token );
 	$self->session( token_secret => $nt->request_token_secret );
@@ -69,6 +83,7 @@ get '/auth_cb' => sub {
 	$self->session( screen_name => $screen_name );
 
 	$self->redirect_to( $self->url_for('index')->to_abs->scheme('https') );
+	#$self->redirect_to( 'index' );
 } => 'auth_cb';
 
 
@@ -106,53 +121,65 @@ post '/upload' => sub {
 	my $image = $self->req->upload('image');
 
 	# Not upload
-	unless ($image) {
+	if ($image->size <= 0) {
 		return $self->render(
 			template => 'error',
-			message  => "ファイルが選択されていません．"
+			message  => "ファイルサイズが0byte以下です。"
 		);
 	}
+
+	# こちらでのチェックをパスしてTwitter側にチェックさせる。
+	# なんていうかもう勘弁して欲しい…
 
 	# Check file type
-	my $image_type = $image->headers->content_type;
-	my %valid_types = map {$_ => 1} qw(image/gif image/jpeg image/png);
+	#my $image_type = $image->headers->content_type;
+	#my %valid_types = map {$_ => 1} qw(image/gif image/jpeg image/png);
 
 	# Content type is wrong
-	unless ($valid_types{$image_type}) {
-		return $self->render(
-			template => 'error',
-			message  => "ファイルタイプが画像ではありません．"
-		);
-	}
+	#unless ($valid_types{$image_type}) {
+	#	return $self->render(
+	#		template => 'error',
+	#		message  => "ファイルタイプが画像ではありません．"
+	#	);
+	#}
 
 	# Extention
-	my $exts = {'image/gif' => 'gif', 'image/jpeg' => 'jpg',
-		'image/png' => 'png'};
-	my $ext = $exts->{$image_type};
+	#my $exts = {'image/gif' => 'gif', 'image/jpeg' => 'jpg',
+	#		'image/png' => 'png'};
+	#my $ext = $exts->{$image_type};
 
 	# Image file
-	my $image_file = "$IMAGE_DIR/" . $screen_name . ".$ext";
+	my $image_file = "$IMAGE_DIR/" . $screen_name . "_" .$image->filename;
 
 	# If file is exists, Retry creating filename
 	while(-f $image_file){
 		my $rand_num = int(rand 100000);
-		$image_file = "$IMAGE_DIR/" . $screen_name . $rand_num . ".$ext";
+		$image_file = "$IMAGE_DIR/" . $screen_name . $rand_num . "_" .$image->filename;
 	}
 
 	# Save to file
 	$image->move_to($image_file);
 
+	$self->stash('name' => $screen_name);
+
+	my $error_occured = "false";
+
 	eval{
 		$nt->update_profile_image([$image_file]);
-		$self->stash('message' => "ファイルをアップロードしました");
 	};
-	if($@) { $self->stash('message' => "ファイルのアップロードに失敗しました（Twitter Error：".$@."）"); }
-	$self->stash('name' => $screen_name);
+	if($@) { 
+		$error_occured = $@;
+	}
+	my $result_message = "ファイルをアップロードしました";
+	if($error_occured ne "false"){
+		$result_message = "ファイルのアップロードに失敗しました（Twitter Error：".$error_occured ."）";
+	}
+	$self->stash('message' => $result_message);
 
 	#ファイル削除
 	unlink $image_file;
-	if($flag == 0) {
 	#セッション削除
+	if($flag == 0) {
 		$self->session( expires => 1 );
 	}
 
@@ -171,5 +198,5 @@ get '/logout' => sub {
 } => 'logout';
 
 app->sessions->secure(1);
-app->secrets(["xxx"]); # セッション管理のために付けておく
+app->secrets([$config->{secure}]); # セッション管理のために付けておく
 app->start;
